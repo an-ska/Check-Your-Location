b0f985bc56be952409e6f665911b2165d904082e
an-ska <1.anna.piotrowska@gmail.com>
2018-07-03 22:30:44 +0200

    Add initial configuration of create-react-app. Add CSS modules.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Test from './components/Test';
+
+class App extends Component {
+  render() {
+    return (
+      <Test/>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Test/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './Test.module.css';
+
+class Test extends Component {
+  render() {
+    return (
+      <p className={styles.testing}>Hello</p>
+    )
+  }
+}
+
+export default Test;
