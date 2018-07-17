5e2e5d9236ebc6435b33dfaedd4de266c00f0860
an-ska <1.anna.piotrowska@gmail.com>
2018-07-17 17:57:05 +0200

    Fix marker display. Add error message when provided IP is correct but location not found.

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -2,19 +2,30 @@ import React from "react";
 import GoogleMapReact from "google-map-react";
 import styles from "./Map.module.css";
 import MapMarker from "../MapMarker";
+import ErrorMessage from "../ErrorMessage";
 
 const Map = ({ latitude, longitude }) => (
   <div className={styles.map}>
-    <GoogleMapReact
-      defaultZoom={11}
-      center={[latitude, longitude]}
-      >
-      <MapMarker
-        latitude={latitude}
-        longitude={longitude}
-        icon="fa-map-marker-alt"
+    {
+      latitude != null || longitude != null
+      ?
+      <GoogleMapReact
+        defaultZoom={11}
+        center={[latitude, longitude]}
+        >
+        <MapMarker
+          lat={latitude}
+          lng={longitude}
+          icon="fa-map-marker-alt"
+        />
+      </GoogleMapReact>
+      :
+      <ErrorMessage
+        icon="fa-frown"
+        text="Sorry, location cannot be found"
       />
-    </GoogleMapReact>
+    }
+
   </div>
 )
 
